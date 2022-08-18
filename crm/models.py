a3937aedbc4c119d9bc4a8ba97b8f0e05e3686ef
Beslan <bes-zaurbaev@yandex.ru>
2022-08-18 10:43:45 +0300

    Admin - add app crm

--- crm/models.py
+++ crm/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Order(models.Model):
-    order_dt = models.DateField(auto_now=True)
-    order_name = models.CharField(max_length=200)
-    order_phone = models.CharField(max_length=200)
+    order_dt = models.DateField(auto_now=True, verbose_name='Дата')
+    order_name = models.CharField(max_length=200, verbose_name='Имя')
+    order_phone = models.CharField(max_length=200, verbose_name='Телефон')
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return self.order_name
--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Order
+
 
 # Create your views here.
+def first_page(request):
+    obj = Order.objects.all()
+    return render(request, './index.html', {'object_list': obj})
