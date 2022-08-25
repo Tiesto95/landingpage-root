b8adfe4e8f51f0ba29f3fe9c539563bfdd4c6dc0
Beslan <bes-zaurbaev@yandex.ru>
2022-08-25 17:22:44 +0300

    Form is Django

--- crm/views.py
+++ crm/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Order
+from .forms import OrderForm
 
 
 # Create your views here.
 def first_page(request):
     obj = Order.objects.all()
-    return render(request, './index.html', {'object_list': obj})
+    form = OrderForm()
+    return render(request, './index.html', {'object_list': obj, 'form': form})
 
 def thanks_page(request):
     name = request.POST['name']
