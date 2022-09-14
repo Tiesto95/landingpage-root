029f2cde6bc64acfa9109be9e3b9435edc29cf8a
Beslan <bes-zaurbaev@yandex.ru>
2022-09-14 11:37:45 +0300

    price app

--- crm/views.py
+++ crm/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from .models import Order
 from .forms import OrderForm
+from cms.models import CmsSlaider
+from price.models import PriceCard, PriceTable
 
 
 # Create your views here.
 def first_page(request):
-    obj = Order.objects.all()
-    form = OrderForm()
-    title = 'Главная страница'
-    return render(request, './index.html', {'object_list': obj, 'form': form, 'title': title})
+    sliders = CmsSlaider.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
+    dict_obj = {
+        'sliders': sliders,
+        'pc_1': pc_1,
+        'pc_2': pc_2,
+        'pc_3': pc_3,
+        'price_table': price_table
+    }
+    return render(request, './index.html', dict_obj)
 
 def thanks_page(request):
     name = request.POST['name']
